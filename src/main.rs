aea826720dadd40dc83f8baf8023e1d778515997
Travis Rodgers <travisdotmedia@gmail.com>
2023-12-29 16:18:14 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+    let ui_handle = ui.as_weak();
+
+    ui.global::<Logic>().on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
+        let num: f64 = string.trim().parse().unwrap();
+        let taxes: f64 = num * TAXPER;
+        let owner: f64 = num * OWNERPER;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPEXPER;
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", {taxes}, {owner}, {profit}, {opex});
+        ui.set_results(result.into());
+    });
+    ui.run()
+}
