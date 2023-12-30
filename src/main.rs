cced211b7b09b39e9fff1af887d7428c3a874081
Travis Rodgers <travisdotmedia@gmail.com>
2023-12-30 13:06:02 -0500

    cleanup and animation added

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ const OPEXPER: f64 = 0.10;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-    let ui_handle = ui.as_weak();
 
-    ui.global::<Logic>().on_divide_income(move |string| {
+    let ui_handle = ui.as_weak();
+    ui.on_divide_income(move |string| {
         let ui = ui_handle.unwrap();
         let num: f64 = string.trim().parse().unwrap();
         let taxes: f64 = num * TAXPER;
@@ -19,5 +19,18 @@ fn main() -> Result<(), slint::PlatformError> {
         let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", {taxes}, {owner}, {profit}, {opex});
         ui.set_results(result.into());
     });
+
     ui.run()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equals_100() {
+        let result = TAXPER + OWNERPER + PROFITPER + OPEXPER;
+        let formatted = f64::trunc(result  * 100.0) / 100.0;
+        assert_eq!(formatted, 1.00);
+    }
 }
