a7409240842c1be1c9038eb30db8bfeb564e3d39
Travis Rodgers <travisdotmedia@gmail.com>
2024-01-02 11:50:31 -0500

    added icons

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), slint::PlatformError> {
         let owner: f64 = num * OWNERPER;
         let profit: f64 = num * PROFITPER;
         let opex: f64 = num * OPEXPER;
-        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", {taxes}, {owner}, {profit}, {opex});
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", taxes, owner, profit, opex);
         ui.set_results(result.into());
     });
 
